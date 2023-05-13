d55d60f069446f62c41ac157bd5cb8919d3acb4a
Rahul <rahul.1rn17ec117@gmail.com>
2023-05-13 15:24:14 +0530

    version 13

--- change.js
+++ change.js
@@ -1,2 +1,3 @@
 console.log('change.js')
 console.log('version 12')
+console.log('version 13')
--- config.js
+++ config.js
@@ -1,3 +1,4 @@
 console.log('config.js')
 console.log('version1')
 console.log('version 12')
+console.log('version 13')
